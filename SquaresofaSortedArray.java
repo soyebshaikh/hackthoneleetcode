a2da4625232a2e264035a540f2ab7ae955e93a48
soyebshaikh <81480317+soyebshaikh@users.noreply.github.com>
2022-11-10 08:40:33 -0800

    Create SquaresofaSortedArray.java

---
+++ SquaresofaSortedArray.java
@@ -0,0 +1,39 @@
+/* 977. Squares of a Sorted Array
+Easy
+6.8K
+170
+Companies
+Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+
+ 
+
+Example 1:
+
+Input: nums = [-4,-1,0,3,10]
+Output: [0,1,9,16,100]
+Explanation: After squaring, the array becomes [16,1,0,9,100].
+After sorting, it becomes [0,1,9,16,100].
+Example 2:
+
+Input: nums = [-7,-3,2,3,11]
+Output: [4,9,9,49,121]*/
+
+
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        for(int i=0;i<nums.length;i++){
+            nums[i]=nums[i]*nums[i];
+        }
+        for(int i=0;i<nums.length;i++){
+            for(int j=i; j<nums.length;j++){
+                if(nums[i]>nums[j]){
+                    int temp=nums[i];
+                    nums[i]=nums[j];
+                    nums[j]=temp;
+                }
+            }
+        }
+        return nums;
+        
+    }
+}
